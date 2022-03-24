bf047da9e6d29cb776b41c09b4a0b923ec311920
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-24 23:06:09 +0600

    cart button added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,13 +3,20 @@ import './Product.css';
 
 const Product = (props) => {
     //console.log(props.product);
-    const {name,img, price } = props.product || {};
+    const { img, name, price, seller, ratings } = props.product || {};
+
     return (
         <div className='product'>
             <img src={img} alt="product-img" />
-            <h2>Name: {name}</h2>
-            <p>price: { price}</p>
-            
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price: {price}</p>
+                <p><small>Manufacture: { seller}</small></p>
+                <p><small>Ratings: { ratings}</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to cart</p>
+            </button>
         </div>
     );
 };
