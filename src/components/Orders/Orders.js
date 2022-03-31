9b0b71c58492b1ccc7a7beb8ac1ec6a03b604bd3
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-31 11:43:33 +0600

    cart added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import useProducts from '../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
     return (
+        
         <div>
             <h2>Welcome to order section!</h2>
+            <p>Total Products :- { products.length}</p>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import useProducts from '../hooks/useProducts';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -29,8 +30,7 @@ const Shop = () => {
             </div>
 
             <div className="cart-container">
-                <h2>Order Summary</h2>
-                <p>Selecter Items: { cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
