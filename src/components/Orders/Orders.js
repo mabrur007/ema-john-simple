8987402e11c36cdccbffda5f50b9cf53c512d82b
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-31 15:40:55 +0600

    remove product from cart

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -7,23 +7,29 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const handleRemoveProduct = product => {
+        // console.log(product);
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+    }
     return (
-        
-        <div>
-            <div className="shop-container">
-                <div className="review-item-container">
-                    {
-                        cart.map(product => <ReviewItem
-                            key={product.id}
-                            product={product}
-                        ></ReviewItem>)
-                    }
-                </div>
-                <div className="cart-container">
-                    <Cart cart={cart}></Cart>
-                </div>
-            </div>
+      <div>
+        <div className="shop-container">
+          <div className="review-item-container">
+            {cart.map((product) => (
+              <ReviewItem
+                key={product.id}
+                product={product}
+                handleRemoveProduct={handleRemoveProduct}
+              ></ReviewItem>
+            ))}
+          </div>
+          <div className="cart-container">
+            <Cart cart={cart}></Cart>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,27 +1,39 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css'
 
 const ReviewItem = (props) => {
-    const {name, img, price, shipping, quantity } = props.product;
+    const { handleRemoveProduct, product } = props;
+    const { name, img, price, shipping, quantity } = product;
+
     return (
-        <div className='review-item'>
-            <div>
-                <img src={img} alt="img" />
-            </div>
-            <div className='review-item-detail-container'>
-                <div className="review-item-details">
-                    <p className='product-name' title={name}>
-                        {name.length > 20 ? name.slice(0, 20) + '...': name}
-                    </p>
-                    <p>Price: <span className='orange'>{price}</span></p>
-                    <p><small>Shipping: {shipping}</small></p>
-                    <p><small>Quantity: {quantity}</small></p>
-                </div>
-                <div className="delete-container">
-                    <button>delete</button>
-                </div>
-            </div>
+      <div className="review-item">
+        <div>
+          <img src={img} alt="img" />
         </div>
+        <div className="review-item-detail-container">
+          <div className="review-item-details">
+            <p className="product-name" title={name}>
+              {name.length > 20 ? name.slice(0, 20) + "..." : name}
+            </p>
+            <p>
+              Price: <span className="orange">${price}</span>
+            </p>
+            <p>
+              <small>Shipping: ${shipping}</small>
+            </p>
+            <p>
+              <small>Quantity: {quantity}</small>
+            </p>
+          </div>
+          <div className="delete-container">
+                <button onClick={() => handleRemoveProduct(product)}>                        
+              <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
+            </button>
+          </div>
+        </div>
+      </div>
     );
 };
 
