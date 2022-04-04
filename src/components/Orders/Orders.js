d9eb78afbf1f7b60cfb235dafde6e250d235bc8c
mabrur007 <mirmd.mabrur@gmail.com>
2022-04-04 19:20:38 +0600

    components edited

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
-import useCart from '../hooks/useCart';
-import useProducts from '../hooks/useProducts';
-import ReviewItem from '../ReviewItem/ReviewItem';
+import React from "react";
+import { Link } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import useCart from "../hooks/useCart";
+import useProducts from "../hooks/useProducts";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import './Order.css';
 
 const Orders = () => {
-    const [products, setProducts] = useProducts();
-    const [cart, setCart] = useCart(products);
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
 
-    const handleRemoveProduct = product => {
-        // console.log(product);
-        const rest = cart.filter(pd => pd.id !== product.id);
-        setCart(rest);
-    }
-    return (
-      <div>
-        <div className="shop-container">
-          <div className="review-item-container">
-            {cart.map((product) => (
-              <ReviewItem
-                key={product.id}
-                product={product}
-                handleRemoveProduct={handleRemoveProduct}
-              ></ReviewItem>
-            ))}
-          </div>
-          <div className="cart-container">
-            <Cart cart={cart}></Cart>
-          </div>
+  const handleRemoveProduct = (product) => {
+    // console.log(product);
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+    removeFromDb(product.id);
+  };
+  return (
+    <div>
+      <div className="shop-container">
+        <div className="review-item-container">
+          {cart.map((product) => (
+            <ReviewItem
+              key={product.id}
+              product={product}
+              handleRemoveProduct={handleRemoveProduct}
+            ></ReviewItem>
+          ))}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart}>
+            <Link to={"/inventory"}>
+              <button>Proceed Check-out</button>
+            </Link>
+          </Cart>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Orders;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import useProducts from "../hooks/useProducts";
 import Product from "../Product/Product";
 import "./Shop.css";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
   const [products, setProducts] = useProducts();
@@ -52,7 +53,11 @@ const Shop = () => {
       </div>
 
       <div className="cart-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart}>
+            <Link to={'/orders'}>
+                <button>Review Order</button>
+            </Link>   
+        </Cart>
       </div>
     </div>
   );
